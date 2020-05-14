c9ebca4bb1840d4e9bc42104d95f4242a78b2ad6
M. Semih Çelek <cybermoon1999@gmail.com>
2020-05-14 17:38:20 +0300

    Tab 1 slider working -Service Added -Needs to improve

---
+++ src/app/feed.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedService {
+  private data = [
+    {
+      category: 'Ekonomik',
+      expanded: true,
+      content:[ 
+        { id:0, inside:'Ekonomi cooohhh eyi'},
+        { id:1, inside:'Ekonomi cooohhh eyi'},
+        { id:2, inside:'Ekonomi cooohhh eyi'},
+        { id:3, inside:'Ekonomi cooohhh eyi'},
+        { id:4, inside:'Ekonomi cooohhh eyi'}
+    ]
+
+    },
+    {
+
+    }
+  ];
+  private feed =[];
+
+  constructor() { }
+
+  getData(){
+    return this.data;
+  }
+  getFeed(){
+    return this.feed;
+  }
+  addFeed(content){
+    this.feed.push(content);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FeedService } from '../feed.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page  implements OnInit{
 
-  constructor() {}
+feed = [];
+items =[];
 
+slideOpts = {
+  centeredSlides: true,
+  slidesPerView: 1.25,
+  spaceBetween: 10,
+
+}
+  constructor(private feedService: FeedService, private router: Router) { }
+
+  ngOnInit(){
+
+    this.feed = this.feedService.getFeed();
+    this.items = this.feedService.getData();
+
+
+  }
 }
